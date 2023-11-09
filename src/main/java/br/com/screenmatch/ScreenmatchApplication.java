5d9611e792d3656b45c53e1754dbc7f30653ac6a
Camila <millanandanet14@gmail.com>
2023-11-09 19:24:39 -0300

    Upload ScreenMatch

---
+++ src/main/java/br/com/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,26 @@
+package br.com.screenmatch;
+
+import br.com.screenmatch.model.DadosTemporada;
+import br.com.screenmatch.principal.Principal;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+        Principal principal = new Principal();
+        principal.exibeMenu();
+
+	}
+}
